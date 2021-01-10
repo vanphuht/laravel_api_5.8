e9ae6bfaec98cfd23e4e4805365cadc2ee1bf667
vanphuht <vanphu.ht@gmail.com>
2021-01-10 15:05:08 +0700

    truoc lenh php artisan make:auth

---
+++ database/migrations/2021_01_10_074719_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 200);
+            $table->string('short_desc', 255);
+            $table->string('desc', 200);
+            $table->string('image', 200);
+            $table->string('post_category_id', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
