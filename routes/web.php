bf119cc3f21bc201f0b072e1dcccc1cf12dd1a05
vanphuht <vanphu.ht@gmail.com>
2021-01-10 15:10:25 +0700

    sau lệnh php artisan make:auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@
 // });
 Route::get('/','HomeController@index');
 Route::get('/bai-viet/{id}','BaivietController@show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
